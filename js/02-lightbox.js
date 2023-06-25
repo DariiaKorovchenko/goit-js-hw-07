76535e2919462cc9bbed8c7aefa541d6ff552844
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-06-26 01:31:09 +0300

    Done Gallery 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItems(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryItems(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href='${original}'>
+         <img class="gallery__image" src='${preview}' alt='${description}' />
+      </a>
+   </li>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  const gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
